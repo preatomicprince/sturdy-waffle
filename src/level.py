77815dbd0fae97c20410212c72ab2df81c48703a
preatomicprince <tom.cafferata@gmail.com>
2023-03-27 15:05:32 +0100

    Corrected camera loop glitch

--- src/level.py
+++ src/level.py
@@ -44,10 +44,13 @@ class Level:
         
         for y in range(len(self.background)): #draw tiles
             for x in self.background[y]:
-                if self.cam.check_on_screen(x.ec):
-                    screen.blit(x.ec.texture, 
-                    (x.ec.rect.x - self.cam.offset.x, x.ec.rect.y - self.cam.offset.y + TOOLBAR_HEIGHT))
-
+                if x.ec.rect.x > 0 or y > 0:
+                    if self.cam.check_on_screen(x.ec):
+                        screen.blit(x.ec.texture, 
+                        (x.ec.rect.x - self.cam.offset.x, x.ec.rect.y - self.cam.offset.y))
+                    if self.cam.offset.x > (COL_COUNT*BG_TILE_SIZE - SCREEN_WIDTH):
+                        screen.blit(x.ec.texture, 
+                        (x.ec.rect.x - self.cam.offset.x + COL_COUNT*BG_TILE_SIZE, x.ec.rect.y - self.cam.offset.y))
         pygame.draw.rect(screen, (255, 255, 255), 
         pygame.Rect(0,0, SCREEN_WIDTH, TOOLBAR_HEIGHT+2)) #draw top toolbar
 
@@ -69,7 +72,7 @@ class Level:
         if self.keys_down.left:
             self.cam.offset.x -= self.cam.speed
 
-        if self.cam.offset.y <= ROW_COUNT*BG_TILE_SIZE - SCREEN_HEIGHT + TOOLBAR_HEIGHT:
+        if self.cam.offset.y <= ROW_COUNT*BG_TILE_SIZE - SCREEN_HEIGHT: #+ TOOLBAR_HEIGHT
             if self.keys_down.down:
                 self.cam.offset.y += self.cam.speed
 
@@ -77,10 +80,10 @@ class Level:
             if self.keys_down.up:
                 self.cam.offset.y -= self.cam.speed
 
-        if  self.cam.offset.x >= ROW_COUNT*BG_TILE_SIZE:
+        if  self.cam.offset.x >= COL_COUNT*BG_TILE_SIZE:
             self.cam.offset.x = 0
         if self.cam.offset.x < 0:
-            self.cam.offset.x = ROW_COUNT*BG_TILE_SIZE
+            self.cam.offset.x = COL_COUNT*BG_TILE_SIZE
 
     def update(self):
         self._update_camera()
