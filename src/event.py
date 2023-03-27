9e2790b88f79adebf9457fceaa9002215d6ccd0e
preatomicprince <tom.cafferata@gmail.com>
2023-03-27 23:21:32 +0100

    Created event.py, moved events func from main

---
+++ src/event.py
@@ -0,0 +1,51 @@
+import pygame
+from level import Level
+def events(level: Level)->None:
+    """this checks for clicks and keyevents"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            sys.exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                level.left_click()
+                print("left\n")
+            if event.button == 3:
+                level.right_click()
+                print("Right\n")
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                level.keys_down.left = True
+            
+                
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                level.keys_down.right = True
+                
+                
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                level.keys_down.up = True
+                
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                level.keys_down.down = True
+   
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            
+            
+        if event.type == pygame.KEYUP:
+        
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                level.keys_down.left = False
+                
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                level.keys_down.right = False
+                
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                level.keys_down.up = False
+                
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                level.keys_down.down = False
+            
--- src/main.py
+++ src/main.py
@@ -5,59 +5,12 @@ from definitions import *
 from level import Level, level_append
 from UI import Buttons, Text
 from resource import resources
-
-def events(level: Level)->None:
-    """this checks for clicks and keyevents"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            sys.exit()
-        
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                level.left_click()
-                print("left\n")
-            if event.button == 3:
-                level.right_click()
-                print("Right\n")
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                level.keys_down.left = True
-            
-                
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                level.keys_down.right = True
-                
-                
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                level.keys_down.up = True
-                
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                level.keys_down.down = True
-   
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-            
-            
-        if event.type == pygame.KEYUP:
-        
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                level.keys_down.left = False
-                
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                level.keys_down.right = False
-                
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                level.keys_down.up = False
-                
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                level.keys_down.down = False
-            
+from event import events
 
 def main()->None:
     """
+    create main variables
+
     Append objects to level
 
     Initialise pygame
@@ -65,11 +18,10 @@ def main()->None:
     main game loop:
         1.Handle events
         2.Update entities
-        3.Draw
+        3.Draw and update display, clock
 
     quit and exit
     """
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     level = Level()
@@ -97,8 +49,9 @@ def main()->None:
 
         pygame.display.update()
         #print(clock.get_fps())
-        clock.tick(FPS) #Can add FPS argument to limit framerate
-        #END GAME LOOP
+        clock.tick(FPS) #Added FPS argument to limit framerate
+
+    #END GAME LOOP
     
 if __name__ == "__main__":
     main()
