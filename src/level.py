be61a5e5ac7ea9c94bdcaba8bd242afc8dc0f255
preatomicprince <tom.cafferata@gmail.com>
2023-03-27 21:15:30 +0100

    Added character movement, mouse left click, ent selection

--- src/level.py
+++ src/level.py
@@ -4,6 +4,7 @@ from background import BG_Tile
 from building import Build_Comp, Building
 from character import Character, Char_Comp
 from resource import resources
+from entity import Ent_Comp, Interact_Comp
 from camera import Camera
 
 class Keys_Down:
@@ -13,6 +14,16 @@ class Keys_Down:
         self.up: bool = 0
         self.down: bool = 0
 
+class Mouse:
+    def __init__(self):
+        self.pos = I_Vec2(0,0)
+        self.ent_ID: int = 0
+        self.ent_type: type
+        # self.buiding: Building
+    
+    def update_pos(self)->I_Vec2:
+        self.pos.x, self.pos.y = pygame.mouse.get_pos()
+        return self.pos
 class Level:
     """Struct to hold all level data and objects """
     def __init__(self)->None:
@@ -21,6 +32,7 @@ class Level:
         self.chars:list(Character) = []
         self.res:dict = resources #count of player resources
         self.cam = Camera()
+        self.mouse = Mouse()
         self.keys_down = Keys_Down()
         self.button_list = []
         self.UI_text = []
@@ -39,8 +51,8 @@ class Level:
         """Draw all entities:
             1. BG_Tiles
             2.UI
-            TODO:
             3. Characters
+            TODO
             4. Buildings"""
 
         screen.fill((0,0,0))
@@ -71,12 +83,20 @@ class Level:
             i.draw(screen)
         
     def _update_char(self, char: Character):
-        if char.cc.aim.x >= 0 and char.cc.aim.y >= 0:
-            char.ec.rect.x += char.cc.vel.x
-            char.ec.rect.y += char.cc.vel.y
-
         if char.cc.aim == char.ec.rect:
             char.cc.aim = I_Vec2(-1, -1)
+        if char.cc.aim.x >= 0 or char.cc.aim.x >= 0:
+            if char.cc.aim.x > char.ec.rect.x:
+                char.ec.rect.x += char.cc.vel.x
+            elif char.cc.aim.x < char.ec.rect.x:
+                char.ec.rect.x += char.cc.vel.x
+
+            if char.cc.aim.y > char.ec.rect.y:
+                char.ec.rect.y += char.cc.vel.y
+            elif char.cc.aim.y < char.ec.rect.y:
+                char.ec.rect.y += char.cc.vel.y
+
+        
 
     def _update_buiding(self, building: Building):
             level.res = self.buildings[i].bc.update_level_res()
@@ -109,6 +129,23 @@ class Level:
             self._update_buidings(building)
         for character in self.chars:
             self._update_char(character)
+
+    def left_click(self):
+        self.mouse.update_pos()
+
+        self.mouse.selected = False
+        self.mouse.ent_ID = None
+        self.mouse.ent_type = None
+
+        for ent in self.chars:
+            if ent.ec.rect.x < self.mouse.pos.x < ent.ec.rect.x + ent.ec.rect.w:
+                if ent.ec.rect.y < self.mouse.pos.y < ent.ec.rect.y + ent.ec.rect.h:
+                    ent.ic.selected = True
+                    self.mouse.ent_ID = ent.ec.ID
+                    self.mouse.ent_type = type(ent)
+        print(f"{self.mouse.ent_ID}, {self.mouse.ent_type}\n")
+        
+
         
 
 
--- src/main.py
+++ src/main.py
@@ -15,7 +15,11 @@ def events(level: Level)->None:
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                print("pressed")
+                level.left_click()
+                """
+                level.mouse.pos.x, level.mouse.pos.y = pygame.mouse.get_pos()
+                new_pos = level.mouse.pos+level.cam.offset
+                print(f"x:{int(new_pos.x/100)%COL_COUNT}, y:{int(new_pos.y/100)%ROW_COUNT}\n")"""
             if event.button == 3:
                 print("right")
         
@@ -77,6 +81,9 @@ def main()->None:
     
     level.add_char("./res/factory.png", I_Vec2(100, 100))
     level.chars[0].cc.aim = I_Vec2(200, 200)
+
+    level.add_char("./res/factory.png", I_Vec2(200, 200))
+    level.chars[1].cc.aim = I_Vec2(300, 350)
 
     house_button = Buttons(100, 620, "button",Path("./res/house_button.png"), 1)
     level.button_list.append(house_button)
